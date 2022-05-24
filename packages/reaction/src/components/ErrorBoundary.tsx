a358d5d71b7c216cce2e7e473c1c1cc3cfa49df0
wener <wenermail@gmail.com>
2022-05-25 02:01:22 +0800

    try fix vercel build error

--- packages/reaction/src/components/ErrorBoundary.tsx
+++ packages/reaction/src/components/ErrorBoundary.tsx
@@ -18,7 +18,7 @@ export interface ErrorBoundaryProps {
  * ErrorBoundary will catch the error and invoke the renderError to render the content
  */
 export class ErrorBoundary extends React.Component<ErrorBoundaryProps, { error?: any }> {
-  static defaultProps = {
+  static defaultProps: ErrorBoundaryProps = {
     onError: ({ error, errorInfo }) => console.error(`ErrorBoundary catch:`, error, errorInfo),
   };
 
