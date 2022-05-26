ee10f74520d821908eafefde76a4205c18dffec8
wener <wenermail@gmail.com>
2022-05-27 03:29:04 +0800

    add html-react-parser demo

--- apps/demo/src/components/page/const.tsx
+++ apps/demo/src/components/page/const.tsx
@@ -3,6 +3,7 @@ import { MdEditNote, MdInsertDriveFile, MdMenu } from 'react-icons/md';
 import { IoCalculator } from 'react-icons/io5';
 import { GrAppsRounded } from 'react-icons/gr';
 import { ImLab } from 'react-icons/im';
+import { SiReact } from 'react-icons/si';
 
 export const NavLinks = [
   {
@@ -18,6 +19,7 @@ export const NavLinks = [
       { icon: <MdEditNote />, label: 'Lexical Editor', href: '/demo/lexical' },
       { icon: <MdMenu />, label: 'Menu', href: '/demo/menu' },
       { icon: <MdInsertDriveFile />, label: 'File Input', href: '/demo/file-input' },
+      { icon: <SiReact />, label: 'html-react-parser', href: '/demo/html-react-parser' },
     ],
   },
   { icon: <VscGithubInverted />, label: 'Github', href: 'https://github.com/wenerme/wode', children: [] },
---
+++ apps/demo/src/pages/demo/html-react-parser.tsx
@@ -0,0 +1,72 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import React from 'react';
+import { useImmer } from 'use-immer';
+import parse, { attributesToProps, domToReact, HTMLReactParserOptions } from 'html-react-parser';
+import { Element, Text } from 'domhandler';
+import { MdAnchor } from 'react-icons/all';
+import classNames from 'classnames';
+
+let initialHtml = `
+<h1>Heading L1</h1><p>Hello</p>
+<h2>Heading L2</h2><p>Hello</p>
+`;
+const Demo = () => {
+  let [state, update] = useImmer({ edit: initialHtml, html: initialHtml });
+  let options: HTMLReactParserOptions = {
+    replace: (dom) => {
+      if (dom instanceof Element) {
+        const props = attributesToProps(dom.attribs);
+        if (dom.children.every((v) => v instanceof Text)) {
+          props.id = dom.children
+            .map((v) => (v as Text).data)
+            .join(' ')
+            .toLowerCase()
+            .replace(/\s+/, '-');
+          props.className = classNames(props.className, 'relative group flex items-center');
+        }
+        switch (dom.name) {
+          case 'h1':
+          case 'h2':
+          case 'h3':
+          case 'h4':
+            return (
+              <dom.name {...props}>
+                {domToReact(dom.children, options)}
+                {props.id && (
+                  <a href={`#${props.id}`} className={'opacity-0 group-hover:opacity-50 ml-2'}>
+                    <MdAnchor className={'inline h-5'} />
+                  </a>
+                )}
+              </dom.name>
+            );
+        }
+      }
+    },
+  };
+  return (
+    <div className={'container mx-auto'}>
+      <div className={'flex mx-auto justify-center max-w-prose gap-2'}>
+        <textarea
+          className={'flex-1 border p-2'}
+          defaultValue={state.html}
+          onBlur={(e) => update({ ...state, html: e.currentTarget.value })}
+        />
+        <div className={'flex-1 border p-2 prose'}>{parse(state.html, options)}</div>
+      </div>
+    </div>
+  );
+};
+
+const CurrentPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Demo Page</title>
+      </Head>
+      <Demo />
+    </>
+  );
+};
+
+export default CurrentPage;
