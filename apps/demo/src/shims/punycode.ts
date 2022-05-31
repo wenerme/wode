2cc4b89f5532b4df1aab2a7b7de19ca8fde74499
wener <wenermail@gmail.com>
2022-06-01 04:53:11 +0800

    ban error for punycode.ts

--- apps/demo/src/shims/punycode.ts
+++ apps/demo/src/shims/punycode.ts
@@ -152,6 +152,7 @@ const basicToDigit = function (codePoint: number) {
 const digitToBasic = function (digit: number, flag: number) {
   //  0..25 map to ASCII a..z or A..Z
   // 26..35 map to ASCII 0..9
+  // @ts-ignore
   return digit + 22 + 75 * (digit < 26) - ((flag != 0) << 5);
 };
 
