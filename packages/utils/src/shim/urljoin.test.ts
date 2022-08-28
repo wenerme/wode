fe941a71332aae33fef5283183026fa98fef8041
wener <wenermail@gmail.com>
2022-08-28 16:00:40 +0800

    jest to ava

--- packages/utils/src/shim/urljoin.test.ts
+++ packages/utils/src/shim/urljoin.test.ts
@@ -1,5 +1,6 @@
 import { urljoin } from './urljoin';
+import test from 'ava';
 
-test('join ext', () => {
-  expect(urljoin('http://wener.me/sub/', '/a/', '/hello.js')).toBe('http://wener.me/sub/a/hello.js');
+test('join ext', (t) => {
+  t.is(urljoin('http://wener.me/sub/', '/a/', '/hello.js'), 'http://wener.me/sub/a/hello.js');
 });
