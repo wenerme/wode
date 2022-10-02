174e6c82dbfb86b9c1a10b7bdf313ccbb0f37805
wener <wenermail@gmail.com>
2022-10-02 15:14:36 +0800

    fix build

--- packages/utils/src/crypto/base.ts
+++ packages/utils/src/crypto/base.ts
@@ -1,7 +1,8 @@
+/**
+ * hex string
+ */
 export function hex(s: Uint8Array | ArrayBuffer) {
   return Array.from(new Uint8Array(s))
     .map((v) => v.toString(16).padStart(2, '0'))
     .join('');
 }
-
-export const base16 = hex;
--- packages/utils/src/crypto/randomUUID.ts
+++ packages/utils/src/crypto/randomUUID.ts
@@ -3,6 +3,9 @@
  */
 export const randomUUID: () => string = globalThis.crypto?.randomUUID || _randomUUID;
 
+/**
+ * @internal
+ */
 export function _randomUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     const r = (Math.random() * 16) | 0;
