993d1cb7614ef95ca2c15f2c341e3cf49e3cf898
wener <wenermail@gmail.com>
2023-01-05 01:47:42 +0800

    apis: downgrade react-router-dom to 6.5.0 workaround next build

--- apps/apis/src/components/WenerApisApp.tsx
+++ apps/apis/src/components/WenerApisApp.tsx
@@ -1,16 +1,50 @@
+import type { ReactNode } from 'react';
 import React from 'react';
+import type { RouteObject } from 'react-router-dom';
 import { createBrowserRouter, createMemoryRouter, RouterProvider } from 'react-router-dom';
 import { ThemeStateReactor } from 'common/src/daisy/theme';
-import { createRouters } from './createRouters';
+import { Router as RemixRouter } from '@remix-run/router';
 
-const router =
-  typeof window === 'undefined' ? createMemoryRouter(createRouters()) : createBrowserRouter(createRouters(), {});
+declare module 'react-router-dom' {
+  export interface IndexRouteObject {
+    meta?: RouteObjectMeta;
+  }
+
+  export interface NonIndexRouteObject {
+    meta?: RouteObjectMeta;
+  }
+}
+
+export interface RouteObjectMeta {
+  id?: string;
+  title?: string;
+  icon?: ReactNode;
+  menu?: Record<string, any>;
+
+  [k: string]: any;
+}
+
+export function createPrimaryRouters(): RouteObject[] {
+  return [
+    {
+      element: <div>Hello</div>,
+    },
+  ];
+}
+
+let router: RemixRouter;
 
 export const WenerApisApp: React.FC = () => {
+  // react-router-dom 6.5.0
+  router ||=
+    typeof window === 'undefined'
+      ? createMemoryRouter(createPrimaryRouters())
+      : createBrowserRouter(createPrimaryRouters(), {});
+
   return (
     <>
       <ThemeStateReactor />
-      <RouterProvider router={router} />
+      {router && <RouterProvider router={router} />}
     </>
   );
 };
