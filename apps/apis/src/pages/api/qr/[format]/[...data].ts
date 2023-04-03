2eaa349d251b11f967b0c3e93e42678f3682ed17
wener <wenermail@gmail.com>
2023-04-03 14:55:17 +0800

    feat: add qr to apis; support png

--- apps/apis/src/pages/api/qr/[format]/[...data].ts
+++ apps/apis/src/pages/api/qr/[format]/[...data].ts
@@ -1,16 +1,19 @@
 import { NextApiHandler, NextApiRequest } from 'next';
 import QRCode from 'qrcode';
+import { z } from 'zod';
 import { arrayOfMaybeArray } from '@wener/utils';
 
 const handler: NextApiHandler = async (req: NextApiRequest, res) => {
   const data = arrayOfMaybeArray(req.query.data).join('/');
-  if (!data) {
+  const params = await QrGenParams.safeParse(req.query);
+  if (!data || !params.success) {
     res.json({
       error: 'invalid data',
     });
     res.status(400).end();
     return;
   }
+
   let buf;
   if (data.startsWith('base64:')) {
     buf = Buffer.from(data.slice(7), 'base64');
@@ -27,11 +30,23 @@ const handler: NextApiHandler = async (req: NextApiRequest, res) => {
     return;
   }
 
-  res.setHeader('Content-Type', 'image/svg+xml');
   let str = buf.toString();
   console.log('QrCode', str);
-  const out = await QRCode.toString(str, { type: 'svg' });
-  res.send(out);
+
+  const { format } = params.data;
+  if (format === 'svg') {
+    res.setHeader('Content-Type', 'image/svg+xml');
+    const out = await QRCode.toString(str, { type: 'svg' });
+    res.send(out);
+  } else if (format === 'png') {
+    res.setHeader('Content-Type', 'image/png');
+    res.send(await QRCode.toBuffer(str, { type: 'png' }));
+  }
 };
 
 export default handler;
+
+const QrGenParams = z.object({
+  format: z.enum(['png', 'svg']),
+  level: z.enum(['low', 'medium', 'quartile', 'high', 'L', 'M', 'Q', 'H']).optional(),
+});
