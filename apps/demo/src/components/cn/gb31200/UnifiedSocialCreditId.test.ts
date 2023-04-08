6b6d9332ef5197656b64b2c44c8561958db7c858
wener <wenermail@gmail.com>
2023-04-08 23:06:51 +0800

    chore: pnpm up fix: reaction jsx preserve -> jsx

--- apps/demo/src/components/cn/gb31200/UnifiedSocialCreditId.test.ts
+++ apps/demo/src/components/cn/gb31200/UnifiedSocialCreditId.test.ts
@@ -1,18 +1,18 @@
-import test from 'ava';
+import { test, assert } from 'vitest';
 import { next, UnifiedSocialCreditId } from './UnifiedSocialCreditId';
 
-test('next', (t) => {
-  t.is(next('ABC', 1), 'ABD');
-  t.is(next('ABC', -1), 'ABB');
-  t.is(next('ABY', +1), 'AC0');
-  t.is(next('AB0', -1), 'AAY');
-  t.is(next('AYY', +1), 'B00');
-  t.is(next('B00', -1), 'AYY');
+test('next', () => {
+  assert.equal(next('ABC', 1), 'ABD');
+  assert.equal(next('ABC', -1), 'ABB');
+  assert.equal(next('ABY', +1), 'AC0');
+  assert.equal(next('AB0', -1), 'AAY');
+  assert.equal(next('AYY', +1), 'B00');
+  assert.equal(next('B00', -1), 'AYY');
 });
 
-test('UnifiedSocialCreditId', (t) => {
+test('UnifiedSocialCreditId', () => {
   const { bureau, subtype, division, subject, sum, valid } = UnifiedSocialCreditId.parse('91310000775785552L');
-  t.deepEqual(
+  assert.deepEqual(
     { bureau, subtype: subtype, division, subject, sum, valid },
     {
       bureau: '9',
@@ -24,6 +24,6 @@ test('UnifiedSocialCreditId', (t) => {
     },
   );
   for (let i = 0; i < 100; i++) {
-    t.true(UnifiedSocialCreditId.random().valid);
+    assert.isTrue(UnifiedSocialCreditId.random().valid);
   }
 });
