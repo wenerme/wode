3fe101c94b344364d7e0f9abc86e0012093cf4f4
wener <wenermail@gmail.com>
2024-05-20 14:51:30 +0800

    nestjs: export BaseNode from type-graphql

--- packages/nestjs/src/Errors.test.ts
+++
@@ -1,8 +0,0 @@
-import { test } from 'vitest';
-import { Errors } from './Errors';
-
-test('Errors', async () => {
-  const a: string | undefined = '';
-  Errors.BadRequest.check(a);
-  a.startsWith('');
-});
--- packages/nestjs/src/type-graphql/index.ts
+++ packages/nestjs/src/type-graphql/index.ts
@@ -1,8 +1,12 @@
+export { BaseNode } from './BaseNode';
 export { BaseObject } from './BaseObject';
+
 export { GraphQLJSONObjectScalar, GraphQLJSONScalar } from './GraphQLJSONScalar';
 export { JSONArgs } from './JSONArgs';
 export { resolveGraphQLJSON } from './resolveGraphQLJSON';
+
 export { NestContainerType } from './NestContainerType';
+
 export { GeneralResponseObject, GeneralResponseResolver } from './GeneralResponseResolver';
 export { GeneralRequestPayload } from './GeneralRequestPayload';
 
@@ -11,7 +15,7 @@ export { ListQueryInput } from './ListQueryInput';
 export { createBaseEntityResolver } from './createBaseEntityResolver';
 export { withBaseQuery } from './withBaseQuery';
 
-export { RelayNode, RelayPageInfo } from './relay';
+export { RelayNode, RelayPageInfo, RelayMutationPayload, RelayMutationInput, runRelayClientMutation } from './relay';
 export type * from './relay';
 
 export type * from './types';
